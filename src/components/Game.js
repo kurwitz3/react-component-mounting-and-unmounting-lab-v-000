244d5ab0f47c2098e1dc6f2a918c0c2bca6bd44a
kurwitz3 <kurwitz3@aol.com>
2021-05-31 22:52:01 -0400

    commit tree 7be053bc07d4bd691d35edd923ef56d6067bec35

--- src/components/Game.js
+++ src/components/Game.js
@@ -15,8 +15,9 @@ class Game extends React.Component {
       raw: 0
     };
   }
-
-  // TODO: create a componentDidMount() which will set the current time
+componentDidMount = () => {
+  this.setCurrentTime()
+}
   
   setCurrentTime = () => {
     this.setState({ time: new Date(Date.now())});
